ce30c0b574ceeeec964308429360d379df846681
Kerem Can <keremcangurann@hotmail.com>
2022-04-07 02:14:46 +0300

    first commit

---
+++ src/main/java/com/kcguran/springreactedu/SpringReactEduApplication.java
@@ -0,0 +1,13 @@
+package com.kcguran.springreactedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactEduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactEduApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kcguran/springreactedu/SpringReactEduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kcguran.springreactedu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactEduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
